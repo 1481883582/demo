13bd7393d2e2775f5523053920bac897aef721e4
yangzhaohui <yangzhaohui@actionsky.com>
2021-11-09 11:37:33 +0800

    更新

---
+++ es_demo/src/main/java/com/api/ESApi.java
@@ -0,0 +1,33 @@
+package com.api;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.entity.Contact;
+import com.es.ContactESService;
+import com.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/es")
+public class ESApi {
+
+    /**
+     * 数据库
+     */
+    @Resource
+    private ContactService contactService;
+
+    @GetMapping("/getList")
+    public Object getRoleList(){
+        List<Contact> list = contactService.list(new LambdaQueryWrapper<Contact>());
+        return list;
+    }
+}
